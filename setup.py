7791b4fbe93e9365521c7a0898cc6a485e35e142
Vadim Liventsev <liventsevvadim@gmail.com>
2018-12-01 19:40:05 +0300

    CookieCutter Data Science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying various random perturbations to sentences in order to train grammar validation models',
+    author='Vadim Liventsev, Mariya Sheyanova, Francis Tyers',
+    license='MIT',
+)
