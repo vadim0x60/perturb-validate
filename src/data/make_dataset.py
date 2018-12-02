26d8ece1833b8a9481f6d648fd87df2a64382df1
Vadim Liventsev <liventsevvadim@gmail.com>
2018-12-02 16:15:08 +0300

    Embeddings downloader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,20 +1,48 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import urllib
+import os
+import nltk
+import tarfile
+from io import BytesIO
+from shutil import copyfileobj
 
+bpemb_bin_url = 'https://nlp.h-its.org/bpemb/ru/ru.wiki.bpe.vs10000.d100.w2v.bin.tar.gz'
+bpemb_bin_name = 'ru.wiki.bpe.vs10000.d100.w2v.bin'
+bpemb_model_url = 'https://nlp.h-its.org/bpemb/ru/ru.wiki.bpe.vs10000.model'
+bpemb_model_name = 'ru.wiki.bpe.vs10000.model'
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(project_dir):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Downloading nltk resources')
 
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger_ru')
+
+    logger.info('Downloading embeddings')
+
+    try:
+        with open(os.path.join(project_dir, 'data', 'external', bpemb_bin_name), 'xb') as f:
+            with urllib.request.urlopen(bpemb_bin_url) as u:
+                buffer = BytesIO(u.read())
+                tar = tarfile.open(fileobj=buffer, mode='r')
+                bin = tar.extractfile(os.path.join('data', 'ru', bpemb_bin_name))
+                copyfileobj(bin, f)
+    except FileExistsError:
+        logger.info(f'{bpemb_bin_name} is already here. Noice! Delete the file and re-run the script to redownload')
+
+    try:
+        with open(os.path.join(project_dir, 'data', 'external', bpemb_model_name), 'xb') as f:
+            with urllib.request.urlopen(bpemb_model_url) as u:
+                copyfileobj(u, f)
+    except FileExistsError:
+        logger.info(f'{bpemb_model_name} is already here. Noice! Delete the file and re-run the script to redownload')
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -27,4 +55,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main(project_dir)
