772af9d65df91d2b485628b5ae554e4d0c28c01a
Vadim Liventsev <liventsevvadim@gmail.com>
2018-12-05 03:30:39 +0300

    Installing ru_punkt

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,13 +6,19 @@ import urllib
 import os
 import nltk
 import tarfile
+import shutil
+from distutils.dir_util import copy_tree
 from io import BytesIO
-from shutil import copyfileobj
+import stat
 
 bpemb_bin_url = 'https://nlp.h-its.org/bpemb/ru/ru.wiki.bpe.vs10000.d100.w2v.bin.tar.gz'
 bpemb_bin_name = 'ru.wiki.bpe.vs10000.d100.w2v.bin'
 bpemb_model_url = 'https://nlp.h-its.org/bpemb/ru/ru.wiki.bpe.vs10000.model'
 bpemb_model_name = 'ru.wiki.bpe.vs10000.model'
+
+def del_readonly(action, name, exc):
+    os.chmod(name, stat.S_IWRITE)
+    os.remove(name)
 
 def main(project_dir):
     """ Runs data processing scripts to turn raw data from (../raw) into
@@ -24,6 +30,14 @@ def main(project_dir):
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger_ru')
 
+    if not any([os.path.isfile(os.path.join(d, 'tokenizers', 'punkt', 'russian.pickle')) for d in nltk.data.path]):
+        logger.info('Downloading russian punctuation model')
+        os.system('git clone https://github.com/Mottl/ru_punkt.git')
+        copy_tree(src=os.path.join(os.getcwd(), 'ru_punkt', 'nltk_data'), 
+                  dst=os.path.join(nltk.data.path[0]))
+            
+        shutil.rmtree(os.path.join(os.getcwd(), 'ru_punkt'), onerror=del_readonly)
+        
     logger.info('Downloading embeddings')
 
     try:
@@ -32,14 +46,14 @@ def main(project_dir):
                 buffer = BytesIO(u.read())
                 tar = tarfile.open(fileobj=buffer, mode='r')
                 bin = tar.extractfile(os.path.join('data', 'ru', bpemb_bin_name))
-                copyfileobj(bin, f)
+                shutil.copyfileobj(bin, f)
     except FileExistsError:
         logger.info(f'{bpemb_bin_name} is already here. Noice! Delete the file and re-run the script to redownload')
 
     try:
         with open(os.path.join(project_dir, 'data', 'external', bpemb_model_name), 'xb') as f:
             with urllib.request.urlopen(bpemb_model_url) as u:
-                copyfileobj(u, f)
+                shutil.copyfileobj(u, f)
     except FileExistsError:
         logger.info(f'{bpemb_model_name} is already here. Noice! Delete the file and re-run the script to redownload')
     
