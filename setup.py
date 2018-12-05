efba888b6e1a4258361873815be3bd26a0ca3ad5
Vadim Liventsev <liventsevvadim@gmail.com>
2018-12-05 04:24:38 +0300

    Modified author list

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Applying various random perturbations to sentences in order to train grammar validation models',
-    author='Vadim Liventsev, Mariya Sheyanova, Francis Tyers',
+    author='Vadim Liventsev, Mariya Sheyanova',
     license='MIT',
 )
