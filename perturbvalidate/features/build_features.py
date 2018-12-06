0017d1dc48bb5147359db17dc41c9f0d7addaebb
Vadim Liventsev <liventsevvadim@gmail.com>
2018-12-06 12:47:27 +0300

    Fixed build_features

--- perturbvalidate/features/build_features.py
+++ perturbvalidate/features/build_features.py
@@ -11,13 +11,13 @@ testament_location = os.path.join(project_dir, 'data', 'raw', 'testament.txt')
 def load_text():
     text = ''
 
-    with open(alg_location, 'r') as f:
-        text = f.read()
+    with open(alg_location, 'r', encoding='utf-8') as f:
+        text += f.read()
 
     text += '\n\n'
 
-    with open(testament_location, 'r') as f:
-        text = f.read()
+    with open(testament_location, 'r', encoding='utf-8') as f:
+        text += f.read()
 
     return text
 
@@ -25,13 +25,14 @@ if __name__ == '__main__':
     transforms = [lambda x: x] + perturbations
     names = ['authentic'] + perturbation_names
 
+    text = load_text()
+    sentences = tokenize(text)
+
     for transform, name in zip(transforms, names):
         try:
-            with open(os.path.join(project_dir, 'data', 'processed', name + '.pickle'), 'x') as f:
-                text = load_text()
-                sentences = tokenize(text)
-                sentences = [transform(sent) for sent in sentences]
-                embeddings = list(embed_sentences(sentences))
+            with open(os.path.join(project_dir, 'data', 'processed', name + '.pickle'), 'xb') as f:
+                transformed_sentences = [transform(sent) for sent in sentences]
+                embeddings = list(embed_sentences(transformed_sentences))
                 pickle.dump(embeddings, f)
         except FileExistsError:
             print(f'{name} dataset already exists')
